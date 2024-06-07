369f1bd96aa5cef20042524bf6d0cfd065b714e3
bartgestel <148961278+bartgestel@users.noreply.github.com>
2024-06-07 10:13:20 +0200

    Started ShipTests

--- containerVervoer_Test/ShipTests.cs
+++ containerVervoer_Test/ShipTests.cs
@@ -1,6 +1,15 @@
-﻿namespace containerVervoer_Test;
+﻿using containerVervoer;
+
+namespace containerVervoer_Test;
 
 public class ShipTests
 {
-    
+    [TestMethod]
+    public void GetClearanceRowsTest()
+    {
+        Ship ship = new Ship(5, 6);
+        int[] expected = {2, 5};
+            
+        CollectionAssert.AreEqual(expected, ship.ClearanceRows);
+    }
 }
