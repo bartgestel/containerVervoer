3ca5ea7c9bd0f1f18f94c1700b134a5f35d25ea8
bartgestel <148961278+bartgestel@users.noreply.github.com>
2024-06-07 10:40:12 +0200

    Added ContainerStack tests

---
+++ containerVervoer_Test/ContainerStackTests.cs
@@ -0,0 +1,32 @@
+﻿using containerVervoer;
+
+namespace containerVervoer_Test;
+
+[TestClass]
+public class ContainerStackTests
+{
+    [TestMethod]
+    public void ContainerWeightCheckTest()
+    {
+        ContainerStack stack = new ContainerStack();
+        Container container = new Container(26000, ContainerType.Normal);
+        
+        stack.AddContainer(container);
+        
+        Assert.IsTrue(stack.ContainerWeightCheck(container));
+    }
+
+    [TestMethod]
+    public void ContainerWeightCheckTestCantStack()
+    {
+        ContainerStack stack = new ContainerStack();
+        Container container = new Container(26000, ContainerType.Normal);
+
+        stack.AddContainer(container);
+        stack.AddContainer(container);
+        stack.AddContainer(container);
+        stack.AddContainer(container);
+        
+        Assert.IsFalse(stack.ContainerWeightCheck(container));
+    }
+}
