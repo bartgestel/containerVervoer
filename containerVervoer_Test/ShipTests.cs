ed21b4d12fb9846f1b33277f84e3d71b04e12588
bartgestel <148961278+bartgestel@users.noreply.github.com>
2024-06-07 10:23:36 +0200

    Made ShipTests

--- containerVervoer_Test/ShipTests.cs
+++ containerVervoer_Test/ShipTests.cs
@@ -2,6 +2,7 @@
 
 namespace containerVervoer_Test;
 
+[TestClass]
 public class ShipTests
 {
     [TestMethod]
@@ -11,5 +12,49 @@ public class ShipTests
         int[] expected = {2, 5};
             
         CollectionAssert.AreEqual(expected, ship.ClearanceRows);
+    }
+
+    [TestMethod]
+    public void LoadCooledContainerTest()
+    {
+        Ship ship = new Ship(5, 6);
+        var containers = new List<Container>{ new Container(26000, ContainerType.Cooled) };
+        
+        ship.LoadCooledContainer(containers);
+        
+        Assert.IsTrue(ship.Rows[0].ContainerStacks[0].Containers.Contains(containers[0]));
+    }
+    
+    [TestMethod]
+    public void LoadNormalContainerTest()
+    {
+        Ship ship = new Ship(5, 6);
+        var containers = new List<Container>{ new Container(26000, ContainerType.Normal) };
+        
+        ship.LoadNormalContainer(containers);
+        
+        Assert.IsTrue(ship.Rows[0].ContainerStacks[0].Containers.Contains(containers[0]));
+    }
+
+    [TestMethod]
+    public void LoadValuableContainerTest()
+    {
+        Ship ship = new Ship(5, 6);
+        var containers = new List<Container>{ new Container(26000, ContainerType.Valuable) };
+        
+        ship.LoadValuableContainers(containers);
+        
+        Assert.IsTrue(ship.Rows[0].ContainerStacks[0].Containers.Contains(containers[0]));
+    }
+    
+    [TestMethod]
+    public void LoadCooledValuableContainerTest()
+    {
+        Ship ship = new Ship(5, 6);
+        var containers = new List<Container>{ new Container(26000, ContainerType.CooledValuable) };
+        
+        ship.LoadCooledValuableContainers(containers);
+        
+        Assert.IsTrue(ship.Rows[0].ContainerStacks[0].Containers.Contains(containers[0]));
     }
 }
